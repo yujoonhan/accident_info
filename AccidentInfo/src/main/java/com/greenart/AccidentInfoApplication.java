c10618cb7078460635057f27563e2587deccb6d7
yujoonhan <sick1925@gmail.com>
2021-08-18 14:38:15 +0900

    새로운 프로젝트 생성

---
+++ AccidentInfo/src/main/java/com/greenart/AccidentInfoApplication.java
@@ -0,0 +1,13 @@
+package com.greenart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccidentInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccidentInfoApplication.class, args);
+	}
+
+}
